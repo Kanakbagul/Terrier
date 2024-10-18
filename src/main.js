c2a954e4463a4376fe751e7d3c6ccf2e0a3dc191
Kanakbagul <bagulkanak123@gmail.com>
2024-10-18 08:16:05 +0000

    save2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log("connnected");
+// console.log("connnected");
+const selectEl = document.querySelector("select");
+const carouselcontainerEl = document.querySelector("carousel-inner");
+console.log (selectEl,carouselcontainerEl);
+
+const BASE_URL = "https://dog.ceo/api/";
+
+async function getDogsList() {
+   const response = await fetch('${BASE_URL)breeds/list/all');
+   const data = await response.json();
+   console.log(response);
+}
+getDogsList();
