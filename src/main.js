0f35890ddb526550384befd1debfb4d5f3cded8d
Kanakbagul <bagulkanak123@gmail.com>
2024-10-18 09:25:44 +0000

    final

--- src/main.js
+++ src/main.js
@@ -1,13 +1,63 @@
-// console.log("connnected");
+// DOM Selection
 const selectEl = document.querySelector("select");
-const carouselcontainerEl = document.querySelector("carousel-inner");
-console.log (selectEl,carouselcontainerEl);
+const carouselContainerEl = document.querySelector(".carousel-inner");
 
 const BASE_URL = "https://dog.ceo/api/";
 
+// === MARK: Fetch
+// Gets the list of dog breeds from api
 async function getDogsList() {
-   const response = await fetch('${BASE_URL)breeds/list/all');
-   const data = await response.json();
-   console.log(response);
+  try {
+    const response = await fetch(`${BASE_URL}breeds/list/all`);
+    const data = await response.json();
+    return Object.keys(data.message);
+  } catch (err) {
+    console.log(err);
+  }
 }
-getDogsList();
+
+// Gets list of 10 images on breed
+async function getDogImages(breed) {
+  try {
+    const res = await fetch(`${BASE_URL}breed/${breed}/images`);
+    const data = await res.json();
+    return data.message.slice(0, 10);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// === MARK: Render
+async function renderOptions() {
+  const breedList = await getDogsList();
+
+  for (breed of breedList) {
+    const option = document.createElement("option");
+    option.textContent = breed[0].toUpperCase() + breed.slice(1).toLowerCase();
+    option.value = breed;
+    selectEl.appendChild(option);
+  }
+}
+
+renderOptions();
+
+async function renderCarousel(breed) {
+  carouselContainerEl.innerHTML = "";
+  const images = await getDogImages(breed);
+
+  for (let i = 0; i < images.length; i++) {
+    const div = document.createElement("div");
+    div.classList.add("carousel-item", i === 0 && "active");
+    div.innerHTML = `<img
+              src="${images[i]}"
+              class="d-block w-100 rounded-3"
+              alt="..."
+            />`;
+    carouselContainerEl.appendChild(div);
+  }
+}
+
+// This runs on select change
+selectEl.addEventListener("change", function (e) {
+  renderCarousel(e.target.value);
+});
